b9b4dadd5d8c91f914a8349526e00a349bc0051f
robinson1990 <n.wataru220@gmail.com>
2013-07-23 21:46:34 +0900

    remove file

--- ImageReconstruction.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDED_IMAGERECONSTRUCTION_H
-#define INCLUDED_IMAGERECONSTRUCTION_H
-#include "C:\OpenCV2.3\include\opencv2\opencv.hpp"
-#include "FractalCode.h"
-void imageReconstruction(FractalCode*,cv::Mat&,const int,cv::Mat&,const int,const int,const int,const int);
-#endif
--- getFractalCodeElements.h
+++
@@ -1,5 +0,0 @@
-#ifndef INCLUDED_GETFRACTALCODEELEMENTS_H
-#define INCLUDED_GETFRACTALCODEELEMENTS_H
-#include "FractalCode.h"
-void getFractalCodeElements(FractalCode*,cv::Mat&,const int,const int,const int,const int);
-#endif
